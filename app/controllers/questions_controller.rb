2cd1390b3094cd2c5c57b7629ab544c359c8a523
Tim Moehrke <tim_moehrke@gmx.de>
2021-05-17 12:49:25 +0200

    questions are being display. style needed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == "I am going to work right now!"
+      @answer = "Great!"
+    elsif params[:question].include? "?"
+      @answer = "Silly question, get dressed and go to work!."
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
